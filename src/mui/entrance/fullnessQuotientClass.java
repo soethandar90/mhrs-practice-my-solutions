81ec533a505249116b4da9cf6b343d3df6672bd4
soethandar90 <soethandar18@gmail.com>
2022-02-26 20:22:41 +0900

    Change function name.

--- src/mui/entrance/fullnessQuotientClass.java
+++ src/mui/entrance/fullnessQuotientClass.java
@@ -1,26 +1,26 @@
 package mui.entrance;
 
-public class smallestFullestClass {
+public class fullnessQuotientClass {
     public static void main(String[] args) {
         //int n = 94;//6
         //int n = 1;//8
         //int n = 9;//5
         //int n = 360;//0
-        int n = -4;//0
-        smallest(n);
+        int n = -4;//-1
+        fullnessQuotient(n);
     }
 
-    static int smallest(int n) {
-        int fullnessQuotient;
+    static int fullnessQuotient(int n) {
+        int fullQuotient;
         if (n > 0) {
-            fullnessQuotient = 8;
+            fullQuotient = 8;
             for (int base = 2; base <= 9; base++) {
                 String rep = "";
                 int tempN = n;
                 while (tempN > 0) {
                     rep += tempN % base;
                     if (String.valueOf(tempN % 2).contains("0")) {
-                        fullnessQuotient--;
+                        fullQuotient--;
                         break;
                     } else {
                         tempN /= base;
@@ -28,9 +28,9 @@ public class smallestFullestClass {
                 }
             }
         } else {
-            fullnessQuotient = -1;
+            fullQuotient = -1;
         }
-        System.out.println(fullnessQuotient);
-        return fullnessQuotient;
+        System.out.println(fullQuotient);
+        return fullQuotient;
     }
 }
