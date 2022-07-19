aacc0d16ac7b5d51c53c186be0dc6599d86d0616
soethandar90 <soethandar18@gmail.com>
2022-07-19 20:29:03 +0900

    Add Interview Practices

--- src/mui/notepad/Validate.java
+++ src/mui/notepad/Validate.java
@@ -1,32 +1,13 @@
 package mui.notepad;
 
-import javax.swing.*;
-import java.util.ArrayList;
-
 public class Validate {
     public static void main(String[] args) {
-        System.out.println(isOddHeavy(new int[]{4, 11, 9, 2, 8}));
-        //System.out.println(isOddHeavy(new int[]{2, 3, 2, 3, 3}));//0
-        //System.out.println(isOddHeavy(new int[] {2, 2, 2, 3, 3, 3}));//0
-        //System.out.println(isOddHeavy(new int[]{2, 2, 1}));//1
-        //System.out.println(isOddHeavy(new int[]{2, 2, 1, 2, 2}));//0
-        //System.out.println(isOddHeavy(new int[]{4, 4, 4, 4, 1,2, 2, 3, 3, 3}));//1
-        //System.out.println(isOddHeavy(new int[]{7, 7, 7, 7, 7, 7, 7, 1}));//1
-        //System.out.println(isOddHeavy(new int[]{7, 7, 7, 7, 1, 7, 7, 7}));//0
-        //System.out.println(isOddHeavy(new int[]{7, 7, 7, 7, 7, 7, 7}));//1
-        //System.out.println(isOddHeavy(new int[]{}));//1
-        //System.out.println(isOddHeavy(new int[]{1, 2, 1}));//0
-        //System.out.println(isOddHeavy(new int[]{2, 1, 1}));//0
-        //System.out.println(isOddHeavy(new int[]{-3, -3, -3}));//0
-        //System.out.println(isOddHeavy(new int[]{0, 2, 2}));//0
-        //System.out.println(isOddHeavy(new int[]{2, 1, 2}));//0
+        //System.out.println(isOddHeavy(new int[]{11, 4, 9, 2, 8}));//1
+
+
     }
 
-    static int isOddHeavy(int[] a){
-
-
-        return 1;
-    }
+    //function
 }
 
 
