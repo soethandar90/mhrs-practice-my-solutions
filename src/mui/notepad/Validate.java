c18a4853dca5381f44a57b75004e9c01e39459fe
soethandar90 <soethandar18@gmail.com>
2022-07-19 12:17:14 +0900

    Add Interview Practices

--- src/mui/notepad/Validate.java
+++ src/mui/notepad/Validate.java
@@ -5,27 +5,26 @@ import java.util.ArrayList;
 
 public class Validate {
     public static void main(String[] args) {
-        System.out.println(isRailroadTie(new int[]{1, 2, 0, 3, -18, 0, 2, 2})); //1
-        System.out.println(isRailroadTie(new int[]{1, 2, 3, 0, 2, 2})); //0
-        System.out.println(isRailroadTie(new int[]{0, 1, 2, 0, 3, 4})); //0
-        System.out.println(isRailroadTie(new int[]{1, 2, 0, 0, 3, 4})); //0
-        System.out.println(isRailroadTie(new int[]{1, 2})); //1
-        System.out.println(isRailroadTie(new int[]{1, 2, 0, 1, 2, 0, 1, 2})); //1
-        System.out.println(isRailroadTie(new int[]{3, 3, 0, 3, 3, 0, 3, 3, 0, 3, 3})); //1
-        System.out.println(isRailroadTie(new int[]{0, 0, 0, 0})); //0
-        System.out.println(isRailroadTie(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9,10})); //0
-        System.out.println(isRailroadTie(new int[]{1, 3, 0, 3, 5, 0})); //0
+        System.out.println(isOddHeavy(new int[]{4, 11, 9, 2, 8}));
+        //System.out.println(isOddHeavy(new int[]{2, 3, 2, 3, 3}));//0
+        //System.out.println(isOddHeavy(new int[] {2, 2, 2, 3, 3, 3}));//0
+        //System.out.println(isOddHeavy(new int[]{2, 2, 1}));//1
+        //System.out.println(isOddHeavy(new int[]{2, 2, 1, 2, 2}));//0
+        //System.out.println(isOddHeavy(new int[]{4, 4, 4, 4, 1,2, 2, 3, 3, 3}));//1
+        //System.out.println(isOddHeavy(new int[]{7, 7, 7, 7, 7, 7, 7, 1}));//1
+        //System.out.println(isOddHeavy(new int[]{7, 7, 7, 7, 1, 7, 7, 7}));//0
+        //System.out.println(isOddHeavy(new int[]{7, 7, 7, 7, 7, 7, 7}));//1
+        //System.out.println(isOddHeavy(new int[]{}));//1
+        //System.out.println(isOddHeavy(new int[]{1, 2, 1}));//0
+        //System.out.println(isOddHeavy(new int[]{2, 1, 1}));//0
+        //System.out.println(isOddHeavy(new int[]{-3, -3, -3}));//0
+        //System.out.println(isOddHeavy(new int[]{0, 2, 2}));//0
+        //System.out.println(isOddHeavy(new int[]{2, 1, 2}));//0
     }
 
-    static int isRailroadTie(int[] a){
-        if((a.length+1)%3!=0 || a[0]==0 || a[a.length-1]==0){
-            return 0;
-        }
-        for(int i=0;i<a.length;i++){
-            if(((i+1)%3==0 && a[i]!=0)||((i+1)%3!=0 && a[i]==0)){
-                return 0;
-            }
-        }
+    static int isOddHeavy(int[] a){
+
+
         return 1;
     }
 }
