8e1937ffa834599b6749dad95b87f32bcb25dcb3
soethandar90 <soethandar18@gmail.com>
2022-07-20 19:43:09 +0900

    Add Interview Practices

--- src/mui/notepad/Validate.java
+++ src/mui/notepad/Validate.java
@@ -2,8 +2,7 @@ package mui.notepad;
 
 public class Validate {
     public static void main(String[] args) {
-        //System.out.println(isOddHeavy(new int[]{11, 4, 9, 2, 8}));//1
-
+        //System.out.println(filterArray(new int[]{8, 4, 9, 0, 3, 1, 2}, 88)); // 0 3 2
 
     }
 
