1732f342edf752e380d3355e4e275ea20e5a6a0b
soethandar90 <soethandar18@gmail.com>
2022-07-25 01:55:36 +0900

    Add Interview Practices

--- src/mui/notepad/Validate.java
+++ src/mui/notepad/Validate.java
@@ -2,11 +2,14 @@ package mui.notepad;
 
 public class Validate {
     public static void main(String[] args) {
-        //System.out.println(filterArray(new int[]{8, 4, 9, 0, 3, 1, 2}, 88)); // 0 3 2
+        System.out.println();
 
     }
 
     //function
+
+
 }
+
 
 
